f450be1d823504734215c450ec3cae2485b77754
paolotrimini <paolo.trimini@gmail.com>
2021-04-16 17:07:21 +0200

    ordinamento genre

--- js/script.js
+++ js/script.js
@@ -10,13 +10,35 @@
     function initVue(){
 
         new Vue({
-
             el: '#app',
             data: {
 
-
+                albums:[],
+                albumGenre:'',
+                orderGenre: [],
             },
 
+            methods: {
+                // BONUS 2: Ordinare i dischi per anno di uscita.
+                myAlbums(albumList){
+                    return albumList.slice().sort((a, b) => parseInt(a.year)  - parseInt(b.year) );
+                }
+            },
+
+            mounted() {
+
+                axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+                    .then((data) => {
+                        this.albums = data.data.response;
+
+                        //BONUS: Creare una select con tutti i generi dei dischi.
+                        this.albums.forEach((item) => {
+                            if (!this.orderGenre.includes(item.genre)) {
+                                this.orderGenre.push(item.genre);
+                            }
+                        });
+                    })
+            },
 
         })
     }
